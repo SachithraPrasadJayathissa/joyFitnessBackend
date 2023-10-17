0f0c5a349ad05e36ba4f74fec88a4f2f32145a7b
SachithraPrasadJayathissa <147978264+SachithraPrasadJayathissa@users.noreply.github.com>
2023-10-17 18:32:09 +0530

    trainer test

--- src/test/java/com/campus/joy_gym/GymMemberControllerTes.java
+++ src/test/java/com/campus/joy_gym/GymMemberControllerTes.java
@@ -1,44 +1,73 @@
 package com.campus.joy_gym;
+
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.campus.joy_gym.controller.GymMemberController;
 import com.campus.joy_gym.payload.entity.GymMember;
-import com.campus.joy_gym.service.GymMemberService;
+import com.campus.joy_gym.repository.GymMemberRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.servlet.MockMvc;
-public class GymMemberControllerTes {
 
+import java.util.Optional;
 
-    @WebMvcTest(GymMemberController.class)
-    public class YourControllerTest {
+public class GymMemberControllerTest {
 
-        @Autowired
-        private MockMvc mockMvc;
+    private GymMemberController gymMemberController;
 
-        @MockBean
-        private GymMemberService gymMemberService;
+    private GymMemberRepository gymMemberRepository;
 
-
-        @MockBean
-        private GymMember gymMember;
-
-        @Test
-        public void testGetAllMembers() throws Exception {
-            ResponseEntity expectedResponse = ResponseEntity.ok(gymMember);
-            when(gymMemberService.getMembers()).thenReturn(expectedResponse);
-
-            mockMvc.perform(get("localhost:8080/member")
-                            .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(status().isOk());
-
-        }
+    @BeforeEach
+    public void setUp() {
+        gymMemberRepository = mock(GymMemberRepository.class);
+        gymMemberController = new GymMemberController();
     }
 
+    @Test
+    public void testUpdateMemberSuccess() {
+        GymMember user = new GymMember();
+
+        when(gymMemberRepository.exitsByNIC(user.getNic()));
+
+        when(gymMemberRepository.save(any(GymMember.class))).thenReturn(user);
+
+        ResponseEntity<?> responseEntity = gymMemberController.updateMemberById(user);
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals("Success", responseEntity.getBody().toString());
+        assertEquals("Successfully Update gym member.", responseEntity.getBody().toString());
+    }
+
+    @Test
+    public void testUpdateMemberNotFound() {
+
+        GymMember user = new GymMember();
+
+
+        OngoingStubbing<Optional<GymMember>> optionalOngoingStubbing = when(gymMemberRepository.exitsByNIC(user.getNic()));
+
+        ResponseEntity<?> responseEntity = gymMemberController.updateMemberById(user);
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertEquals("Failed", responseEntity.getBody().toString());
+        assertEquals("There are no user in this ID : " + user.getNic(), responseEntity.getBody().toString());
+    }
+
+    @Test
+    public void testUpdateMemberError() {
+        GymMember user = new GymMember();
+
+        OngoingStubbing<Optional<GymMember>> optionalOngoingStubbing = when(gymMemberRepository.exitsByNIC(user.getNic()));
+
+        when(gymMemberRepository.save(any(GymMember.class))).thenThrow(RuntimeException.class);
+
+        ResponseEntity<?> responseEntity = gymMemberController.updateMemberById(user);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals("Error", responseEntity.getBody().toString());
+        assertEquals("An error occurred while updating gym member.", responseEntity.getBody().toString());
+    }
 }
